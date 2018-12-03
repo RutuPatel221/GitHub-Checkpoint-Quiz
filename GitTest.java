f5b21b0a52f79a85eb9d6630710b67f18e8b7456
RutuPatel221 <45392239+RutuPatel221@users.noreply.github.com>
2018-12-03 07:54:03 -0500

    Constructor Added
    A single instance variable and default constructor that makes the instance variable equal to "This is my GitHub Test"is created.

---
+++ GitTest.java
@@ -0,0 +1,13 @@
+public class GitTest{
+
+	private String outprint;
+
+	public GitTest(){
+		outprint = "This is my GitHub test";
+	}
+
+	public GitTest(String outprint){
+		this.outprint = outprint;
+	}
+
+}
